ddfa3113c8478853302d02f10d2455494ded5615
Girik soni <giriksoni0710@gmail.com>
2020-01-18 17:24:41 -0800

    working frontend of website

--- js/functions.js
+++ js/functions.js
@@ -10,10 +10,10 @@ jQuery(function($) {
 	window.onscroll = function() {myFunction()};
 		
 	var navbar = document.getElementById("navbar");
-	var sticky = navbar.offsetTop;
+	// var sticky = navbar.offsetTop;
 	
 	function myFunction() {
-	  if (window.pageYOffset >= sticky) {
+	  if (window.pageYOffset >= 30) {
 		navbar.classList.add("sticky")
 	  } else {
 		navbar.classList.remove("sticky");
